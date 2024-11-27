bfe19af575d2229f31007f7ed7963ab3a845b745
mdparizeau <6michaels@gmail.com>
2024-11-26 22:18:07 -0800

    npc dialogue

---
+++ Assets/Scripts/dialogueManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class dialogueManager : MonoBehaviour
+{
+
+    public TMP_Text nameText;
+    public TMP_Text dialogueText;
+
+    public Animator animator;
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue (dialogue Dialogue)
+    {
+        animator.SetBool("isOpen", true);
+
+        nameText.text = Dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in Dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("isOpen", false);
+        Debug.Log("End of conversation.");
+    }
+
+}
---
+++ Assets/Scripts/dialogueTrigger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogueTrigger : MonoBehaviour
+{
+    public dialogue Dialogue;
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<dialogueManager>().StartDialogue(Dialogue);
+    }
+}
